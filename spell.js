7da0a1588d8077455c0d331a04ed93cb2031a98a
Dig0-M <d_mendezsantos@u.pacific.edu>
2026-01-21 17:57:49 -0800

    I dont have to load the JS file, simplier to just make them into HTMLs!

--- spell.js
+++ spell.js
@@ -14,13 +14,3 @@ function castSpell() {
                 "Your First cast: " + (t2 - t1).toFixed(1) + " ms\n" +
                 "Your Second cast: " + (t3 - t2).toFixed(1) + " ms";
 }
-
-
-//Added to make sure my other js scrolls and quests can easily become buttons
-function loadSQScript(src) {
-            if(document.querySelector(`script[src="${src}"]`)) {return; }
-            const script = document.createElement("script");
-            script.src = src;
-            script.defer = true;
-            document.body.appendChild(script);
-}
